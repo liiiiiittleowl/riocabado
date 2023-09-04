42955a4098a634d46c91c6d9d62200de3b268c08
Owl <15622568067@163.com>
2023-09-05 01:01:36 +0800

    feat(core.impl): 添加SingletonCore

--- core.impl.go
+++ core.impl.go
@@ -1,5 +1,45 @@
 package riocabado
 
+import "sync"
+
+
+
+
+type SingletonCore[T any] struct{
+	value T
+	has bool
+
+	locker sync.Locker
+	getter func() (value T, e error)
+}
+func(the *SingletonCore[T]) GetValue() (value T, e error) {
+	if the.has {return the.value, nil;}
+
+	the.locker.Lock();
+	defer the.locker.Unlock();
+
+	if the.has {
+		return the.value, nil;
+	} else {
+		value, e = the.getter();
+		if e != nil {return *new(T), e;}
+
+		the.value = value;
+		the.has = true;
+		return value, nil;
+	}
+}
+
+func Singleton[T any](getter func() (value T, e error)) Core[T] {
+	return &SingletonCore[T]{
+		value: *new(T),
+		has: false,
+
+		locker: new(sync.Mutex),
+		getter: getter,
+	};
+}
+
 
 
 
